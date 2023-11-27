9654658d59455654b4775cb7029c838f546635ce
Anuj123verma <anuj17026@iiitd.ac.in>
2023-11-27 23:29:40 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const preview = false
 const folder = path.join(__dirname, "data")
 
 try{
-    const data = fs.readdir(folder, (err, data)=>{
+    fs.readdir(folder, (err, data)=>{
         for(let idx =0;idx<data.length;idx++){
             const item = data[idx];
             let oldFile = path.join(folder,item)
             let newFile = path.join(folder, item.replaceAll(replaceThis, replaceWith))
-            if(!preview){
+            if(!preview && oldFile!=newFile){
                 fs.rename(oldFile, newFile, ()=>{
                     console.log("Rename Success", item, newFile)
                 })
